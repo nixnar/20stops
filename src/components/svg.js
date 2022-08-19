8378d6b477f265726b83a732e98f911bc1dff232
tarafanlin <tarafanlin@gmail.com>
2022-08-18 17:02:05 -0700

    assets

---
+++ src/components/svg.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+export const ArrowUpRight = (props) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+      style={{ display: "block", ...props.style }}
+    >
+    <path d="M4.66669 11.3333L11.3334 4.66666" stroke="#00050A" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4.66669 4.66666H11.3334V11.3333" stroke="#00050A" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  );
