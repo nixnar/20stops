00d72fa1fd3f1f1af0510ebcc53313dd8d369ce7
Ran X <aldousxin1@gmail.com>
2024-01-30 17:43:34 -0800

    added logos

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,13 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
-import * as SVG from "../components/svg";
+import * as SVG from "../components/svg"
 
 const links = [
   {
     text: "Reel",
     url: "https://www.youtube.com/watch?v=zgBO-dcmQco",
-    description: "Reel is currently out of date, new reel work in progress"
+    description: "Reel is currently out of date, new reel work in progress",
   },
   {
     text: "Stills",
@@ -33,28 +33,45 @@ const samplePageLinks = [
 ]
 
 const brandlogos = [
-  { filepath: "https://i.imgur.com/TMx2kcj.png",
-    width: '120px'},
-  { filepath: "https://i.imgur.com/PL20VBh.png",
-  width: '40px',
-  height: '60px' },
+  { filepath: "https://i.imgur.com/TMx2kcj.png", width: "115px" },
+  {
+    filepath: "https://i.imgur.com/PL20VBh.png",
+    width: "40px",
+    height: "60px",
+  },
+  //cosmo
+  {
+    filepath: "https://i.imgur.com/0bxKi1P.png",
+    width: "120px",
+  },
   { filepath: "https://i.imgur.com/d4UnJry.png" },
   { filepath: "https://i.imgur.com/LNkvraa.png" },
-  { filepath: "https://i.imgur.com/k4ZcXeC.png",
-    width: '120px'},
+  { filepath: "https://i.imgur.com/k4ZcXeC.png", width: "120px" },
   { filepath: "https://i.imgur.com/dtrjFj2.png" },
+  { filepath: "https://i.imgur.com/C4YH9l3.png", width: "100px" },
+  //horn
+  { filepath: "https://i.imgur.com/z5Y4IO2.png" },
   { filepath: "https://i.imgur.com/TjAjoH4.png" },
+  //matty
+  { filepath: "https://i.imgur.com/SKjB24I.png", width: "90px" },
   { filepath: "https://i.imgur.com/vYXhlIU.png" },
+
   { filepath: "https://i.imgur.com/HMUmann.png" },
-  { filepath: "https://i.imgur.com/TG69k3q.png",
-    width: '52px',
-    height: '52px' },
-  { filepath: "https://i.imgur.com/rgiJfNq.png",
-    width: '64px',
-    height: '64px' },
-  { filepath: "https://i.imgur.com/j2v4imq.png",     
-    width: '68px',
-    height: '68px' },
+  {
+    filepath: "https://i.imgur.com/TG69k3q.png",
+    width: "52px",
+    height: "52px",
+  },
+  {
+    filepath: "https://i.imgur.com/rgiJfNq.png",
+    width: "64px",
+    height: "64px",
+  },
+  {
+    filepath: "https://i.imgur.com/j2v4imq.png",
+    width: "68px",
+    height: "68px",
+  },
 ]
 
 const moreLinks = [
@@ -66,17 +83,26 @@ const moreLinks = [
   {
     text: "Instagram",
     url: "https://www.instagram.com/ranxin20stops/",
-  }
+  },
 ]
 
 const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Ran Xin" />
+    <Seo title="Home" />
     <div className={styles.intro}>
       <h1>
-      Ran Xin <span className={styles.textGray}>is a Cinematographer, Colorist. <br />Currently based in San Francisco. <br />He has over 8 years of work experience on feature films, short films, commercials, pitch decks, and weddings.<br />He provides full service in house production, delivering video and image content for a fraction of what competing market charge.</span>
+        Ran Xin{" "}
+        <span className={styles.textGray}>
+          is a Cinematographer, Colorist. <br />
+          Currently based in San Francisco. <br />
+          He has over 8 years of work experience on feature films, short films,
+          commercials, pitch decks, and weddings.
+          <br />
+          He provides full service in house production, delivering video and
+          image content for a fraction of what competing market charge.
+        </span>
       </h1>
     </div>
 
@@ -84,11 +110,16 @@ const IndexPage = () => (
     <div className={styles.flex}>
       {brandlogos.map(logo => (
         <React.Fragment key={logo.filepath}>
-          <img className={styles.img} style={{width: logo.width, height: logo.height}} src={logo.filepath} layout="constrained" placeholder="blurred"/>
+          <img
+            className={styles.img}
+            style={{ width: logo.width, height: logo.height }}
+            src={logo.filepath}
+            layout="constrained"
+            placeholder="blurred"
+          />
         </React.Fragment>
       ))}
     </div>
-
     <ul className={styles.list}>
       {links.map(link => (
         <li key={link.url} className={styles.listItem}>
@@ -97,7 +128,17 @@ const IndexPage = () => (
             href={`${link.url}${utmParameters}`}
             target={"_blank"}
           >
-          <h1><div className={styles.flex}>{link.text} <div className={styles.arrowDesktop}><SVG.ArrowUpRight height={36}/></div><div className={styles.arrowMobile}><SVG.ArrowUpRight height={28}/></div></div></h1>
+            <h1>
+              <div className={styles.flex}>
+                {link.text}{" "}
+                <div className={styles.arrowDesktop}>
+                  <SVG.ArrowUpRight height={36} />
+                </div>
+                <div className={styles.arrowMobile}>
+                  <SVG.ArrowUpRight height={28} />
+                </div>
+              </div>
+            </h1>
           </a>
           <p className={styles.listItemDescription}>{link.description}</p>
         </li>
@@ -106,7 +147,13 @@ const IndexPage = () => (
 
     {moreLinks.map((link, i) => (
       <React.Fragment key={link.url}>
-        <a className={styles.listItemLink} href={`${link.url}${utmParameters}`} target={"_blank"}>{link.text}</a>
+        <a
+          className={styles.listItemLink}
+          href={`${link.url}${utmParameters}`}
+          target={"_blank"}
+        >
+          {link.text}
+        </a>
         {i !== moreLinks.length - 1 && <> · </>}
       </React.Fragment>
     ))}
